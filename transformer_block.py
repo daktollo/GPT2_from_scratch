5be335ce7ec6253ec63c18acbb9e3eba5a3a2c15
daktollo <mevlutcanadiyanan@gmail.com>
2025-05-11 15:22:10 +0300

    feat: Implement core components of the GPT model including LayerNorm, GELU, FeedForward, and TransformerBlock classes

---
+++ transformer_block.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from layers import FeedForward, LayerNorm
+from multihead_attention import MultiHeadAttention
+
+class TransformerBlock(nn.Module):
+    def __init__(self, conf):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=conf["emb_dim"],
+            d_out=conf["emb_dim"],
+            context_length=conf["context_length"],
+            num_heads=conf["n_heads"], 
+            dropout=conf["drop_rate"],
+            qkv_bias=conf["qkv_bias"])
+        self.ff = FeedForward(conf)
+        self.norm1 = LayerNorm(conf["emb_dim"])
+        self.norm2 = LayerNorm(conf["emb_dim"])
+        self.drop_shortcut = nn.Dropout(conf["drop_rate"])
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        return x
